3d97ad3bab2981a92b3588c1dcc192b34db17f78
degenRobot <86513395+degenRobot@users.noreply.github.com>
2024-02-27 16:30:27 -0700

    (minor adj to fix compilation)

--- src/script/deployStallion.s.sol
+++ src/script/deployStallion.s.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import {Strategy} from "./Strategy.sol";
-import {fantasyOracle} from "./FantasyOracle.sol";
-import {fantasyFactory} from "./FantasyFactory.sol";
+import {Strategy} from "../Strategy.sol";
+import {fantasyOracle} from "../FantasyOracle.sol";
+import {fantasyFactory} from "../FantasyFactory.sol";
 
 import {Script, console} from "forge-std/Script.sol";
 
@@ -12,28 +12,30 @@ contract DeployStallion is Script {
     address public asset = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
 
     function deployFantasyOracle() internal returns (address) {
-        address oracle = new fantasyOracle(admin);
-        console.log("Deployed Fantasy Oracle at ", oracle);
-        return oracle;
+        fantasyOracle oracle = new fantasyOracle(admin);
+        console.log("Deployed Fantasy Oracle at ", address(oracle));
+        return address(oracle);
     }
 
     function deployFantasyFactory(address _oracle) internal returns (address) {
-        address factory = new fantasyFactory(admin, _oracle);
-        console.log("Deployed Fantasy Factory at ", factory);
-        return factory;
+        fantasyFactory factory = new fantasyFactory(admin, _oracle);
+        console.log("Deployed Fantasy Factory at ", address(factory));
+        return address(factory);
     }
 
+    /*
     function deployStrategy() internal returns (address) {
         address strategy = new Strategy(address(asset), "Tokenized Strategy");
         console.log("Deployed Strategy at ", strategy);
         return strategy;
     }
+    */
 
     function run() external {
         admin = msg.sender;
         address oracle = deployFantasyOracle();
         address factory = deployFantasyFactory(oracle);
-        deployStrategies(factory);
+        //deployStrategy();
 
         // Can also set up a game here ??? 
 
